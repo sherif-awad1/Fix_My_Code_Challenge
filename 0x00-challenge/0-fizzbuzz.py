a84fd81a02815f5c9896ec75ad2348738196f37c
Sherif Awad <sherif.hero1@gmail.com>
2024-03-30 14:28:20 -0700

    edit commit

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -8,9 +8,9 @@ def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
 
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
         return
